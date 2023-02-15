bfd4d5bc197ea32ee901ff7010ab38254d137bb4
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2023-02-15 22:58:31 +0530

    register route implemented

--- Leyfit-backend/models/truck.model.js
+++ Leyfit-backend/models/truck.model.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const Renew = new Schema({
+  lastUpdated: String,
+  expiry: String,
+});
+
 const TruckSchema = new Schema({
-  truckNumner: {
+  truckNumber: {
     type: String,
     unique: true,
     required: [true, "truck number is required "],
@@ -10,6 +15,41 @@ const TruckSchema = new Schema({
   password: {
     type: String,
     required: [true, "password is required "],
+  },
+  brakes: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  lights: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  wheels: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  engineOil: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  wipers: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  generalMaintenance: {
+    type: Map,
+    // of: Renew,
+    required: true,
+  },
+  puc: {
+    type: Map,
+    // of: Renew,
+    required: true,
   },
 });
 
